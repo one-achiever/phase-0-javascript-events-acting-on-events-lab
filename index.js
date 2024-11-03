92591bd806b314b8534c019a1012cdd404aff30c
linder <achie_31@yahoo.com>
2024-11-03 17:00:09 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) { // Check to ensure the dodger doesn't move out of bounds on the left
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) { // Adjust the boundary as needed based on container width
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
